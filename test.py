38c43747494fe1dd6a597cd5bc88d31c9cf4b137
WAZ <waz100k@gmail.com>
2020-05-18 11:18:12 -0300

    nada

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 import os
 import exifread
-
+# conda install -c conda-forge exifread
 
 def walk_through_files(path, file_extensions=['jpg','jpeg']):
-  for (dirpath, dirnames, filenames) in os.walk(path):
+  for (dirpath, _, filenames) in os.walk(path):
     for filename in filenames:
       for extension in file_extensions:
         if filename.endswith(extension):
           yield os.path.join(dirpath, filename)
 
 def get_tags(filename):
-  f = open(filename, 'rb')
-  return exifread.process_file(f)
-
+  with open(filename, 'rb') as f:
+    return exifread.process_file(f)
 
 for fname in walk_through_files("."):
   print(fname)
