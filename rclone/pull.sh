062df683cd03a7836e6a07f2cbfe207808520e45
Wenceslao A. Zuloaga <waz100k@gmail.com>
2024-02-29 13:50:45 +0100

    arguments $ format

--- rclone/pull.sh
+++ rclone/pull.sh
@@ -2,10 +2,9 @@
 
 DOIT=${2:-dontdoit}
 
-if [ -z "$1" ];
-then
-    echo "usage: $0 REMOTE [doit]";
-    exit 1;
+if [ -z "$1" ]; then
+    echo "usage: $0 REMOTE [doit]"
+    exit 1
 fi
 
 REMOTE=${1%/}
@@ -13,18 +12,19 @@ REMOTE=${1%/}
 SRC="$REMOTE:/"
 DST="$REMOTE"
 
-NOW="`date +%Y-%m-%d_%H.%M`"
+NOW="$(date +%Y-%m-%d_%H.%M)"
 BKP="$DST.debris/$NOW"
 
+[ -d "$DST" ] || {
+    echo "missing dir $DST"
+    exit 666
+}
 
-[ -d "$DST" ] || { echo "missing dir $DST" ; exit 666 ; }
-
-CMD="rclone sync $SRC $DST/ --bwlimit-file 2M --transfers 2 --checksum --backup-dir=$BKP -v --log-file $BKP/rclone.log --progress"
+CMD="rclone sync $SRC $DST/ --track-renames --bwlimit-file 1M --transfers 2 --checksum --backup-dir=$BKP -v --log-file $BKP/rclone.log --progress"
 # --stats 60s
-if [ "$DOIT" != 'doit' ]
-then
-    echo "missing [doit] => executing dry-run mode";
-    CMD="$CMD --dry-run";
+if [ "$DOIT" != 'doit' ]; then
+    echo "missing [doit] => executing dry-run mode"
+    CMD="$CMD --dry-run"
 fi
 
 echo -e "\n$CMD"
@@ -40,4 +40,4 @@ echo "cleanup small files in $BKP"
 echo "cleanup empty dirs in $BKP"
 # find "$BKP/" -type d -empty -print -delete
 
-ps fax|grep rcl[o]ne
+ps fax | grep rcl[o]ne
--- sync.py
+++ sync.py
@@ -1,4 +1,4 @@
-#! python3
+#!/bin/env python3
 
 import os
 import argparse
@@ -7,12 +7,13 @@ from datetime import datetime
 from decouple import config
 
 parser = argparse.ArgumentParser()
-parser.add_argument("target", help="target dir ie: [hostname:]/path")
 parser.add_argument("origin", help="origin dir", nargs="?", default=config("base_dir"))
+parser.add_argument("target", help="target dir ie: [hostname:]/path")
 parser.add_argument("trash", help="trash dir", nargs="?", default=config("trash"))
+parser.add_argument("doit", help="any string (if missing will run rsync with '--dry-run')", nargs="?", default="")
 args = parser.parse_args()
 
-trash = args.trash + "/" + datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+trash = args.target + args.trash + "/" + datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
 
 os.makedirs(args.trash, exist_ok=True)
 
@@ -28,10 +29,17 @@ cli_parmas = [
     "--backup",
     "--backup-dir=" + trash,
     "--itemize-changes",
-    # "--dry-run",
+    # "--delete-after",
+    "--delete-delay",
     args.origin,
     args.target,
 ]
+
+if args.doit == "":
+    cli_parmas += ["--dry-run"]
+
+
+print(" ".join(cli_parmas) + f" >{trash}.log")
 
 with open(f"{trash}.log", "w") as output:
 
