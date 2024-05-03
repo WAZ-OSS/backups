de8bd988de672a494101df1aaebfbe86b2526e8b
Wenceslao A. Zuloaga <waz100k@gmail.com>
2024-05-03 09:11:45 +0200

    added loging on rclone-pull-all

--- rclone-pull-all.sh
+++ rclone-pull-all.sh
@@ -12,7 +12,14 @@ DONTASK=${3:-ask}
 
 cd "$REMOTESDIR"
 
-for REMOTE in */ ; do
-    echo "$REMOTE"
-    rclone-pull.sh "$REMOTE" "$DOIT" "$DONTASK" || echo -e "\n####################\n[ERROR] pulling $REMOTE returns code: $?\n####################\n"
+LOGSDIR="$REMOTESDIR/.debris"
+mkdir -p "$LOGSDIR"
+NOW="$(date +%Y-%m-%d_%H.%M)"
+LOGFILE="$LOGSDIR/$NOW-$(basename "$0").log"
+
+for REMOTE in */; do
+    echo "[$(date +'%Y-%m-%d %H:%M:%S %Z')] $REMOTE START"
+    SECONDS=0
+    rclone-pull.sh "$REMOTE" "$DOIT" "$DONTASK" >>"$LOGFILE" || echo -e "[ERROR] code: $?"
+    echo "[$(date +'%Y-%m-%d %H:%M:%S %Z')] $REMOTE DONE (took $(TZ=UTC0 printf '%(%H:%M:%S)T' "$SECONDS"))"
 done
