74487b3b442a82f22e57857ac6a630528af62d0f
Wenceslao A. Zuloaga <waz100k@gmail.com>
2024-05-03 11:05:20 +0200

    +pull-all crontab support

--- rclone-pull-all.sh
+++ rclone-pull-all.sh
@@ -1,14 +1,21 @@
 #!/bin/env bash
 set -e
 
+cycleMinutes=1440 # 1 day
+
 if [ -z "$1" ]; then
-    echo "usage: $0 /REMOTES/BASE/DIRECTORY/ [doit] [dontask]"
+    echo "usage: $0 /REMOTES/BASE/DIRECTORY/ [doit] [dontask] [cycleMinutes=$cycleMinutes]"
+    echo "for usage inside crontab:"
+    echo "- sync will be silently skipped if there is a sync log newer than \$cycleMinutes(default: $cycleMinutes) ago."
+    echo -e "\nie - add in crontab to run each hour and only update sync each 6 hours:"
+    echo -e "\n0 * * * * $0 /REMOTES_DIRECTORY doit dontask 360"
     exit 1
 fi
-
-REMOTESDIR="${1:-somewhere}"
+REMOTESDIR=${1:-somewhere}
+REMOTESDIR=${REMOTESDIR%/}
 DOIT=${2:-dontdoit}
 DONTASK=${3:-ask}
+MINUTES=${4:-$cycleMinutes}
 
 cd "$REMOTESDIR"
 
@@ -16,6 +23,14 @@ LOGSDIR="$REMOTESDIR/.debris"
 mkdir -p "$LOGSDIR"
 NOW="$(date +%Y-%m-%d_%H.%M)"
 LOGFILE="$LOGSDIR/$NOW-$(basename "$0").log"
+
+RECENTLOGS=$(find "$LOGSDIR" -iname "*-$(basename "$0").log" -mmin "-$MINUTES")
+if [ "$RECENTLOGS" != "" ]
+then
+    # echo "already ran less than $MINUTES minutes ago:"
+    # echo "$RECENTLOGS"
+    exit 0
+fi
 
 for REMOTE in */; do
     echo "[$(date +'%Y-%m-%d %H:%M:%S %Z')] $REMOTE START"
